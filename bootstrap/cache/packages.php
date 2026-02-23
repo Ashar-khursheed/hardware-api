5df0eda85a06125e27212c623424a7c4b571eca3
Ashar-khursheed <asharkhursheed26@gmail.com>
2026-02-24 01:30:53 +0500

    Removed Twilio secret

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,25 +1,43 @@
 <?php return array (
   'barryvdh/laravel-dompdf' => 
   array (
+    'aliases' => 
+    array (
+      'PDF' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
+      'Pdf' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
+    ),
     'providers' => 
     array (
       0 => 'Barryvdh\\DomPDF\\ServiceProvider',
     ),
+  ),
+  'brotzka/laravel-dotenv-editor' => 
+  array (
     'aliases' => 
     array (
-      'Pdf' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
-      'PDF' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
+      'DotenvEditor' => 'Brotzka\\DotenvEditor\\DotenvEditorFacade',
+    ),
+    'providers' => 
+    array (
+      0 => 'Brotzka\\DotenvEditor\\DotenvEditorServiceProvider',
     ),
   ),
   'cartalyst/stripe-laravel' => 
   array (
+    'aliases' => 
+    array (
+      'Stripe' => 'Cartalyst\\Stripe\\Laravel\\Facades\\Stripe',
+    ),
     'providers' => 
     array (
       0 => 'Cartalyst\\Stripe\\Laravel\\StripeServiceProvider',
     ),
-    'aliases' => 
+  ),
+  'codezero/laravel-unique-translation' => 
+  array (
+    'providers' => 
     array (
-      'Stripe' => 'Cartalyst\\Stripe\\Laravel\\Facades\\Stripe',
+      0 => 'CodeZero\\UniqueTranslation\\UniqueTranslationServiceProvider',
     ),
   ),
   'cviebrock/eloquent-sluggable' => 
@@ -31,24 +49,13 @@
   ),
   'intervention/image' => 
   array (
-    'providers' => 
-    array (
-      0 => 'Intervention\\Image\\ImageServiceProvider',
-    ),
     'aliases' => 
     array (
       'Image' => 'Intervention\\Image\\Facades\\Image',
     ),
-  ),
-  'jackiedo/dotenv-editor' => 
-  array (
     'providers' => 
     array (
-      0 => 'Jackiedo\\DotenvEditor\\DotenvEditorServiceProvider',
-    ),
-    'aliases' => 
-    array (
-      'DotenvEditor' => 'Jackiedo\\DotenvEditor\\Facades\\DotenvEditor',
+      0 => 'Intervention\\Image\\ImageServiceProvider',
     ),
   ),
   'laravel/sail' => 
@@ -81,13 +88,20 @@
   ),
   'maatwebsite/excel' => 
   array (
+    'aliases' => 
+    array (
+      'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
+    ),
     'providers' => 
     array (
       0 => 'Maatwebsite\\Excel\\ExcelServiceProvider',
     ),
-    'aliases' => 
+  ),
+  'matanyadaev/laravel-eloquent-spatial' => 
+  array (
+    'providers' => 
     array (
-      'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
+      0 => 'MatanYadaev\\EloquentSpatial\\EloquentSpatialServiceProvider',
     ),
   ),
   'mll-lab/laravel-graphiql' => 
@@ -99,13 +113,13 @@
   ),
   'mollie/laravel-mollie' => 
   array (
-    'providers' => 
-    array (
-      0 => 'Mollie\\Laravel\\MollieServiceProvider',
-    ),
     'aliases' => 
     array (
       'Mollie' => 'Mollie\\Laravel\\Facades\\Mollie',
+    ),
+    'providers' => 
+    array (
+      0 => 'Mollie\\Laravel\\MollieServiceProvider',
     ),
   ),
   'nesbot/carbon' => 
@@ -136,13 +150,14 @@
       0 => 'Nuwave\\Lighthouse\\LighthouseServiceProvider',
       1 => 'Nuwave\\Lighthouse\\Async\\AsyncServiceProvider',
       2 => 'Nuwave\\Lighthouse\\Auth\\AuthServiceProvider',
-      3 => 'Nuwave\\Lighthouse\\Cache\\CacheServiceProvider',
-      4 => 'Nuwave\\Lighthouse\\GlobalId\\GlobalIdServiceProvider',
-      5 => 'Nuwave\\Lighthouse\\OrderBy\\OrderByServiceProvider',
-      6 => 'Nuwave\\Lighthouse\\Pagination\\PaginationServiceProvider',
-      7 => 'Nuwave\\Lighthouse\\SoftDeletes\\SoftDeletesServiceProvider',
-      8 => 'Nuwave\\Lighthouse\\Testing\\TestingServiceProvider',
-      9 => 'Nuwave\\Lighthouse\\Validation\\ValidationServiceProvider',
+      3 => 'Nuwave\\Lighthouse\\Bind\\BindServiceProvider',
+      4 => 'Nuwave\\Lighthouse\\Cache\\CacheServiceProvider',
+      5 => 'Nuwave\\Lighthouse\\GlobalId\\GlobalIdServiceProvider',
+      6 => 'Nuwave\\Lighthouse\\OrderBy\\OrderByServiceProvider',
+      7 => 'Nuwave\\Lighthouse\\Pagination\\PaginationServiceProvider',
+      8 => 'Nuwave\\Lighthouse\\SoftDeletes\\SoftDeletesServiceProvider',
+      9 => 'Nuwave\\Lighthouse\\Testing\\TestingServiceProvider',
+      10 => 'Nuwave\\Lighthouse\\Validation\\ValidationServiceProvider',
     ),
   ),
   'prettus/l5-repository' => 
@@ -154,13 +169,13 @@
   ),
   'spatie/laravel-ignition' => 
   array (
-    'providers' => 
-    array (
-      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
-    ),
     'aliases' => 
     array (
       'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
     ),
   ),
   'spatie/laravel-medialibrary' => 
@@ -177,15 +192,22 @@
       0 => 'Spatie\\Permission\\PermissionServiceProvider',
     ),
   ),
-  'srmklive/paypal' => 
+  'spatie/laravel-translatable' => 
   array (
     'providers' => 
     array (
-      0 => 'Srmklive\\PayPal\\Providers\\PayPalServiceProvider',
+      0 => 'Spatie\\Translatable\\TranslatableServiceProvider',
     ),
+  ),
+  'srmklive/paypal' => 
+  array (
     'aliases' => 
     array (
       'PayPal' => 'Srmklive\\PayPal\\Facades\\PayPal',
+    ),
+    'providers' => 
+    array (
+      0 => 'Srmklive\\PayPal\\Providers\\PayPalServiceProvider',
     ),
   ),
 );
