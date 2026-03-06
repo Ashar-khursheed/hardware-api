29b0f7d98ca7ed02611313f13911916f5dcfbf67
Ashar-khursheed <asharkhursheed26@gmail.com>
2026-03-06 20:20:59 +0500

    sd

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 // Countries & States
-Route::apiResource('state', 'App\Http\Controllers\StateController');
+// Route::apiResource('state', 'App\Http\Controllers\StateController');
 Route::apiResource('country', 'App\Http\Controllers\CountryController');
 
 // Settings & Theme Options
